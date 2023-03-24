9a2b11e07d3d41a2c43f3dc79eb49db493b75ae5
Techchhiv <techchhivlim5@gmail.com>
2023-03-24 21:32:06 +0700

    Create Landing.java

---
+++ Landing.java
@@ -0,0 +1,17 @@
+package Quiz01;
+import java.util.Scanner;
+public class Landing {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String option; 
+        System.out.println("Welcome to my Menu");
+        System.out.println("Please choose option below");
+        System.out.println("1.Regiseration\n2.Login\n3.Reset Password\n4.Typing\n5.Result list");
+        option = sc.next();
+        switch (option){
+            case 1:
+                Register.main(args);
+                break;
+        }
+    }
+}
