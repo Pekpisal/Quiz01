1a860af6f6742422f2b2b4f82fe219a7d334ff3d
Techchhiv <127384271+Techchhiv@users.noreply.github.com>
2023-03-24 21:59:04 +0700

    Update ResetPass.java

--- ResetPass.java
+++ ResetPass.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
 
-public class ResetPw {
+public class ResetPass{
     public static void main(String[] args) {
         System.out.println("Reset password\n");
         Scanner sc = new Scanner(System.in);
-        String Old, New; 
+        String pass1, pass2; 
         System.out.print("Insert old password:");
-        Old = sc.nextLine();
+        pass1 = sc.nextLine();
         System.out.print("Insert new password:");
-        New = sc.nextLine();
+        pass2 = sc.nextLine();
         System.out.println("The password is resetted");
+        sc.close();
     }
 }
